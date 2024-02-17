6eea038c0a16ea292eeae311a4971328d76ba308
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2024-02-17 10:21:48 +0700

    Config Program Add Folder and Config Appsettings

---
+++ be-artwork-shraing-platform/be-artwork-sharing-platform/be-artwork-sharing-platform/Core/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace be_artwork_sharing_platform.Core.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
